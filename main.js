47629cd00c058d18c480bfeea5196b3ae7ac4b31
Mateo Senoner <mateo.senoner@student.uibk.ac.at>
2018-04-11 17:26:04 +0200

    Quellcode erstellt

---
+++ main.js
@@ -0,0 +1,29 @@
+//alert("Hallo Welt");
+
+let nachricht = "Hallo Welt";
+//alert (Nachricht);
+nachricht = "neuer Text";
+//alert (nachricht);
+console.log(nachricht);
+const linkColor = "#ff0000";
+let eine_variable = "test";
+console.log("Variable: eine_variable: ", eine_variable);
+let highscore = 354.5
+console.log("highscore:", highscore/20);
+
+let meineNachricht ="test1";
+console.log(meineNachricht)
+meineNachricht = "test2";
+console.log(meineNachricht)
+meineNachricht = `Eine mehr
+und
+ein
+ganz
+langer
+text`;
+console.log (meineNachricht);
+
+let isOver18 = true;
+console.log("Über 18?", isOver18);
+let age = 17;
+console.log("age über 18", age > 18);
